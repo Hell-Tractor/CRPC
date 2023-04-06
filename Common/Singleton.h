1d5b5f9fa7f11007a654250ab6caecb88758a4d9
Hell-Tractor <1104840448@qq.com>
2023-04-06 08:40:27 +0800

    添加单例模式基类代码

---
+++ Common/Singleton.h
@@ -0,0 +1,27 @@
+#pragma once
+
+template <typename T>
+class singleton {
+    private:
+        static T* _instance = nullptr;
+    protected:
+        singleton() = default;
+    public:
+        singleton& operator = (const singleton&) const = delete;
+        singleton& operator = (const singleton&&) const = delete;
+        singleton(const singleton&) = delete;
+        singleton(singleton&&) = delete;
+    
+        static T* instance() {
+            if (_instance == nullptr)
+                _instance = new T();
+            return _instance;
+        }
+
+        static void destroy() {
+            if (_instance != nullptr) {
+                delete _instance;
+                _instance = nullptr;
+            }
+        }
+};
