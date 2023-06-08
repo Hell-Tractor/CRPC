c04a58509e39d0b7be6ae14be418b3d49ca98ca8
xumingyi <22920202202931@stu.xmu.edu.cn>
2023-06-09 02:47:20 +0800

    fixed server

--- Server/server.h
+++ Server/server.h
@@ -19,6 +19,11 @@ namespace crpc {
     class server final : public std::enable_shared_from_this<server> {
     private:
         uint16_t _port;
+        
+        std::unique_ptr<asio::io_context> _io_context;
+        std::unique_ptr<asio::ip::tcp::acceptor> _acceptor;
+        std::unique_ptr<asio::signal_set> _signals;
+
         std::unordered_map<std::string, std::function<awaitable<std::string>()>> _methods;
 
     public:
@@ -37,7 +42,7 @@ namespace crpc {
         // 处理单个客户端会话的协程
         awaitable<void> client_session(asio::ip::tcp::socket socket) {
             const std::string addr_str = socket.remote_endpoint().address().to_string() + ":" + std::to_string(socket.remote_endpoint().port());
-            LOGGER.log_info("client({}) session started", addr_str);
+            LOGGER.log_info("<{}> client session started", addr_str);
 
             uint32_t current_seq_id = 0;
 
@@ -46,32 +51,34 @@ namespace crpc {
                     // 接受请求
                     proto::package request;
                     co_await request.await_read_from(socket);
-                    LOGGER.log_debug("recv package from client({}): {}", addr_str, request.brief_info());
+                    LOGGER.log_debug("<{}> recv package from client: {}", addr_str, request.brief_info());
 
                     if (request.type() == proto::request_type::RPC_METHOD_REQUEST) {
                         // 方法调用请求
                         std::string name = request.data();
-                        LOGGER.log_info("method request from client({}): {}", addr_str, name);
+                        LOGGER.log_info("<{}> method request from client: {}", addr_str, name);
 
                         std::string response_msg;
                         auto it = _methods.find(name);
                         if (it == _methods.end()) {
                             response_msg = std::format("no handler for {}", name);
+                            LOGGER.log_info("<{}> method no found for client: {}", addr_str, name);
                         } else {
                             response_msg = co_await it->second();
+                            LOGGER.log_info("<{}> method done for client: {}", addr_str, name);
                         }
-                        LOGGER.log_debug("method done for client({}): {}", addr_str, name);
                         // 响应
                         proto::package response(proto::request_type::RPC_METHOD_RESPONSE, request.seq_id() + 1u, response_msg);
                         co_await response.await_write_to(socket);
+                        LOGGER.log_debug("<{}> send package to client: {}", addr_str, response.brief_info());
                     }
                 }
             }
             catch (std::exception& e) {
-                LOGGER.log_error("exception in client session: {}", e.what());
+                LOGGER.log_error("<{}> exception in client session: {}", addr_str, e.what());
             }
 
-            LOGGER.log_info("client({}) session closed", addr_str);
+            LOGGER.log_info("<{}> client session closed", addr_str);
         }
 
         // 监听协程
@@ -79,11 +86,10 @@ namespace crpc {
         {
             auto self = shared_from_this();
             auto executor = co_await asio::this_coro::executor;
-            asio::ip::tcp::acceptor acceptor(executor, { asio::ip::tcp::v4(), _port });
             LOGGER.log_info("server listening on port {}", _port);
             while (true) {
                 // 接受客户端连接 生成客户端会话协程
-                asio::ip::tcp::socket socket = co_await acceptor.async_accept(asio::use_awaitable);
+                asio::ip::tcp::socket socket = co_await _acceptor->async_accept(asio::use_awaitable);
                 asio::co_spawn(executor, [self, &socket] { return self->client_session(std::move(socket)); }, asio::detached);
             }
         }
@@ -92,11 +98,13 @@ namespace crpc {
         void start(uint16_t port) {
             auto self = shared_from_this();
             _port = port;
-            asio::io_context io_context(1);
-            asio::signal_set signals(io_context, SIGINT, SIGTERM);
-            signals.async_wait([&](auto, auto) { io_context.stop(); });
-            asio::co_spawn(io_context, [self] { return self->listener(); }, asio::detached);
-            io_context.run();
+            _io_context = std::make_unique<asio::io_context>(1);
+            _acceptor = std::make_unique<asio::ip::tcp::acceptor>(*_io_context, asio::ip::tcp::endpoint(asio::ip::tcp::v4(), _port));
+            _signals = std::make_unique<asio::signal_set>(*_io_context, SIGINT, SIGTERM);
+
+            _signals->async_wait([&](auto, auto) { _io_context->stop(); });
+            asio::co_spawn(*_io_context, [self] { return self->listener(); }, asio::detached);
+            _io_context->run();
         }
     };
 
